803791f54f4995c9a4ae3d22a6e3a1730eb3a8d6
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-06-03 17:44:08 +0200

    Bump Clojure version to support new implementation and linters

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/henrygarner/biscuit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :plugins [[lein-cljfmt "0.6.2"]
             [jonase/eastwood "0.3.4"]])
