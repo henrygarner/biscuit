67237e9ee6cd1697232f619cb2538feae865bad1
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-06-03 17:38:53 +0200

    Add formatting and linting checkers

--- project.clj
+++ project.clj
@@ -4,3 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
+  :plugins [[lein-cljfmt "0.6.2"]
+            [jonase/eastwood "0.3.4"]])
